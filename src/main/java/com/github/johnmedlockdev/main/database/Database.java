516cb684f5a3fb898c4f0c5d193a84a9065b54b0
John Medlock <42301475+johnMedlockDev@users.noreply.github.com>
2020-04-22 16:10:59 -0500

    created properties file. added driver for postgress. created var statements

--- src/main/java/com/github/johnmedlockdev/main/database/Database.java
+++ src/main/java/com/github/johnmedlockdev/main/database/Database.java
@@ -1,7 +1,32 @@
 package com.github.johnmedlockdev.main.database;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
 public class Database {
     public Database(String fileName) {
 
     }
+
+    private void getSql() throws IOException, SQLException {
+
+        FileReader reader = new FileReader("db.properties");
+        Properties p = new Properties();
+        p.load(reader);
+        String user = p.getProperty("user");
+        String password = p.getProperty("password");
+        String url = p.getProperty("url");
+        String getAll = p.getProperty("selectAll");
+
+        Connection conn = DriverManager.getConnection(url, user, password);
+
+        Statement stmt = conn.prepareStatement(getAll);
+    }
+
+
 }
