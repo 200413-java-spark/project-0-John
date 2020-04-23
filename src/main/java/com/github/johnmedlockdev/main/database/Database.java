60bd08b214961553f9e78146fd0940be30b7983b
John Medlock <42301475+johnMedlockDev@users.noreply.github.com>
2020-04-23 13:14:09 -0500

    connects and writes to database. refactoring a little.

--- src/main/java/com/github/johnmedlockdev/main/database/Database.java
+++ src/main/java/com/github/johnmedlockdev/main/database/Database.java
@@ -7,37 +7,71 @@ import java.util.Properties;
 
 public class Database {
 
-    private final String propertiesFile = "src/main/java/com/github/johnmedlockdev/main/database/db.properties";
-
     public Database(String fileName) throws SQLException, IOException, ClassNotFoundException {
         getSql();
     }
 
     private void getSql() throws IOException, SQLException, ClassNotFoundException {
 
+        String propertiesFile = "src/main/java/com/github/johnmedlockdev/main/database/db.properties";
         FileReader reader = new FileReader(propertiesFile);
         Properties p = new Properties();
         p.load(reader);
 
+//      db connection setup
         String username = p.getProperty("username");
         String password = p.getProperty("password");
         String url = p.getProperty("url");
-        String getAll = p.getProperty("selectAll");
 
-        Class.forName(p.getProperty("postgresDriver"));
+//      prepared statement assignment
+        String selectAll = p.getProperty("selectAll");
+        String insertStatement = p.getProperty("insertStatement");
+
+////////////////////////////////////////////////////////////////////////////////
+        Connection conn = DriverManager.getConnection(url, username, password);
+////////////////////////////////////////////////////////////////////////////////
+
+//      prepared statement setup
+        PreparedStatement stmt = conn.prepareStatement(insertStatement); // input sql statement with ?
+
+        stmt.setString(2,"spy"); // set params based of what they are
+        stmt.setDouble(3,54.34); // set params based of what they are
+
+//      Results set obj
+        ResultSet rs = stmt.executeQuery();
+
+//        rs.next(); // you have to do this to get off the headers
+//        System.out.println(rs.getInt(1)+" "+rs.getString(2) + " "+ rs.getDouble(3));
+
+        rs.close(); // close connection
+        stmt.close(); // close connection
+        conn.close(); // close connection
+
+//        prints query
+//        ResultSet resultSet = statement.executeQuery(getAll);
+//        ResultSetMetaData rsmd = resultSet.getMetaData();
+//        int columnsNumber = rsmd.getColumnCount();
+//        while (resultSet.next()) {
+//            for (int i = 1; i <= columnsNumber; i++) {
+//                if (i > 1) System.out.print(", ");
+//                String columnValue = resultSet.getString(i);
+//                System.out.print(columnValue + " " + rsmd.getColumnName(i));
+//            }
+//            System.out.println();
+//        }
 
 
-        Connection db = DriverManager.getConnection(url,username, password);
-        Statement st = db.createStatement();
-        ResultSet rs = st.executeQuery(getAll);
+//        stuff to learn
+//        results.getString(arg) .getInt(arg) statement.execute() preparedstatement(statement) executeQuery
+//        System.out.println(rs.getMetaData()); => prints obj
 
-        while (rs.next()) {
-            System.out.print("Column 1 returned ");
-            System.out.println(rs.getString(1));
-        }
-        rs.close();
-        st.close();
+
     }
 
+private void readData(){
 
+}
+private void writeData(){
+
+}
 }
